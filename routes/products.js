5e317a5dbe2cf72b945ab1713d2c88336ba46042
unknown <dikedaniel7917@gmail.com>
2024-08-08 13:26:58 +0100

    created route to get all products

--- routes/products.js
+++ routes/products.js
@@ -68,9 +68,7 @@ router.get("/", async(req, res) => {
     }else {
         products = await Product.find()
     }
-
-
-    res.status(200).json(users); 
+    res.status(200).json(products); 
   } catch (error) {
     res.status(500).json(error)
   }
